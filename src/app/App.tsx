fd1371ab326020f0b1795d4f5a1a749b9f60e72a
Artem Bondarchuk <arhipilagys@gmail.com>
2026-01-12 01:47:21 +1100

    added redirect to /vacancies

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import { Layout } from "./Layout";
 import { VacanciesPage } from "../pages/VacanciesPage";
 import { ProfilePage } from "../pages/ProfilePage";
@@ -10,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="vacancies" replace />} />
           <Route path="vacancies" element={<VacanciesPage />} />
           <Route path="profile" element={<ProfilePage />} />
           <Route path="vacancies/:id" element={<VacancyPage />} />
